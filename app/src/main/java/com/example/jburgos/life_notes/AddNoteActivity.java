bdb81ece66e8f5d98ebc281f3e5a341ead792117
Jose Burgos <jburgos7705@gmail.com>
2018-08-03 09:42:32 -0400

    Reverted back to aactivities Capstone Stage 2 Begin

--- app/src/main/java/com/example/jburgos/life_notes/AddNoteActivity.java
+++ app/src/main/java/com/example/jburgos/life_notes/AddNoteActivity.java
@@ -1,56 +1,139 @@
 package com.example.jburgos.life_notes;
 
-import android.app.Fragment;
-import android.os.Build;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentStatePagerAdapter;
-import android.support.v4.view.ViewPager;
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.jburgos.life_notes.ViewModel.AddNoteViewModel;
+import com.example.jburgos.life_notes.ViewModel.AddNoteViewModelFactory;
+import com.example.jburgos.life_notes.data.AppDatabase;
+import com.example.jburgos.life_notes.data.NoteEntry;
+import com.example.jburgos.life_notes.utils.AppExecutors;
+
+import java.util.Date;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class AddNoteActivity extends AppCompatActivity {
 
-    // Extra for the note ID to be received in the intent
+    // Extra for the task ID to be received in the intent
     public static final String EXTRA_NOTE_ID = "extraNoteId";
+    // Extra for the task ID to be received after rotation
+    public static final String NOTE_INSTANCE_ID = "instanceOfNoteId";
 
+    // Constant for default task id to be used when not in update mode
+    private static final int DEFAULT_TASK_ID = -1;
+    // Constant for logging
+    private static final String TAG = AddNoteActivity.class.getSimpleName();
 
+    //Member Variable for database
+    private AppDatabase database;
 
-    //@BindView(R.id.pager)
-    //ViewPager mPager;
-    //private MyPagerAdapter mPagerAdapter;
-    @BindView(R.id.up_container)
-    View mUpButtonContainer;
-    @BindView(R.id.action_up)
-    View mUpButton;
+    // Fields for views
+    @BindView(R.id.editTextNote)
+    EditText mEditText;
+    @BindView(R.id.saveButton)
+    Button mButton;
 
-    @Override
+    private int mTaskId = DEFAULT_TASK_ID;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_note);
         ButterKnife.bind(this);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            getWindow().getDecorView().setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
-                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+
+        setSaveButtonClick();
+
+        //initialize database
+        database = AppDatabase.getInstance(getApplicationContext());
+
+        if (savedInstanceState != null && savedInstanceState.containsKey(NOTE_INSTANCE_ID)) {
+            mTaskId = savedInstanceState.getInt(NOTE_INSTANCE_ID, DEFAULT_TASK_ID);
         }
 
-        if (savedInstanceState == null) {
-            FragmentManager fragmentManager = getSupportFragmentManager();
+        Intent intent = getIntent();
+        if (intent != null && intent.hasExtra(EXTRA_NOTE_ID)) {
+            mButton.setText(R.string.update_button);
+            if (mTaskId == DEFAULT_TASK_ID) {
+                // populate the UI
+                mTaskId = intent.getIntExtra(EXTRA_NOTE_ID, DEFAULT_TASK_ID);
 
-            Bundle bundle = getIntent().getExtras();
+                AddNoteViewModelFactory factory = new AddNoteViewModelFactory(database, mTaskId);
+                final AddNoteViewModel viewModel
+                        = ViewModelProviders.of(this, factory).get(AddNoteViewModel.class);
 
-            NoteFragment notesDetailFragment = new NoteFragment();
-            notesDetailFragment.setArguments(bundle);
-            fragmentManager.beginTransaction()
-                    .add(R.id.note_container, notesDetailFragment)
-                    .commit();
-
+                //Observe the LiveData object in the ViewModel. Use it also when removing the observer
+                viewModel.getNote().observe(this, new Observer<NoteEntry>() {
+                    @Override
+                    public void onChanged(@Nullable NoteEntry noteEntry) {
+                        viewModel.getNote().removeObserver(this);
+                        populateUI(noteEntry);
+                    }
+                });
+            }
         }
+    }
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt(NOTE_INSTANCE_ID, mTaskId);
+        super.onSaveInstanceState(outState);
+    }
+
+    /**
+     * setSaveButtonClick is called from onCreate to init the onclick for the save button
+     */
+    private void setSaveButtonClick() {
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onSaveButtonClicked();
+            }
+        });
+    }
+
+    private void populateUI(NoteEntry note) {
+
+        if (note == null) {
+            return;
+        }
+
+        mEditText.setText(note.getDescription());
+
+    }
+
+    /**
+     * onSaveButtonClicked is called when the "save" button is clicked.
+     * It retrieves user input and inserts that new task data into the underlying database.
+     */
+    public void onSaveButtonClicked() {
+        String description = mEditText.getText().toString();
+        Date date = new Date();
+
+        final NoteEntry note = new NoteEntry(description, date);
+        AppExecutors.getInstance().diskIO().execute(new Runnable() {
+            @Override
+            public void run() {
+                if (mTaskId == DEFAULT_TASK_ID) {
+                    // insert new task
+                    database.noteDao().insertNotes(note);
+                } else {
+                    //update task
+                    note.setId(mTaskId);
+                    database.noteDao().updateNotes(note);
+                }
+                finish();
+            }
+        });
     }
 
 }
